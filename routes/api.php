b84cb5ffd556eb8c344dd41576325ba4eb1eaf72
Oscar Diaz <o-pipe@hotmail.com>
2019-04-05 22:44:29 -0500

    Se instalada vuejs usando webpack

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('blog','BlogController')->except('create','edit', 'update', 'destroy');
+
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('blog','BlogController')->except('create','edit', 'update', 'destroy');
+
